aff4166203fe47524486822c84a1c1572b75685e
asakasamo <a.sakasamo@gmail.com>
2018-08-09 18:59:58 -0500

    Start out with a blank slate

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template>   
+   <div id="app">
+      <header>
+         <h1>Prisoner's Dilemma Meets Conway's Game of Life</h1>
+      </header>
+
+      <div>
+         
+      </div>
+
+      <router-view/>
+   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+   overflow: hidden;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+body {
+   background-color: #ddd;
+   padding: 0;
+   margin: 0;
+}
+
+#app {
+   font-family: "Avenir", Helvetica, Arial, sans-serif;
+   text-align: center;
+   color: white;
+}
+
+header {
+   background-color: #555;
 }
 </style>
